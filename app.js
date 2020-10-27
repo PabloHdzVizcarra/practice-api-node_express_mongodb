6d74ee053c7363732bd83e6085572b0683d0dd66
Pablo Hernandez <pablourbanohdzvizcarra@gmail.com>
2020-10-26 21:38:11 -0600

    Feat: added cors
    cors is configured to work only with path "/api"

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
 const router = require('./router/router')
+const cors = require('cors')
 
 app.use(express.static(__dirname + "/public"))
-app.use('/', router())
+
+app.use('/api', cors())
+app.use('/api', router())
+
+app.use('/', (req, res) => {
+  res.send("Hello welcome")
+})
 
 module.exports = app
--- router/router.js
+++ router/router.js
@@ -7,11 +7,10 @@ module.exports = () => {
   router.get('/api', handler.start)
 
 
-  router.get('/api/vacations', handler.getVacations)
-  router.get('/api/vacation/:sku', handler.getVacation)
-  router.post('/api/vacation/:sku/notify-when-in-season', handler.sendEmail)
-  router.delete('/api/vacation/:sku', handler.deleteVacation)
-
+  router.get('/vacations', handler.getVacations)
+  router.get('/vacation/:sku', handler.getVacation)
+  router.post('/vacation/:sku/notify-when-in-season', handler.sendEmail)
+  router.delete('/vacation/:sku', handler.deleteVacation)
 
   return router
 }
